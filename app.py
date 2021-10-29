6c76eaf28bbb45e5e4a2f439f6e0fabfd71673fc
Atharva Shirdhankar <72031540+StarTrooper08@users.noreply.github.com>
2021-10-29 17:46:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ app = Flask(__name__)
 def index():
     transcript = ''
     if request.method == "POST":
-         print("FORM DATA RECEIVED")
+         print("FORM DATA RECEIVED") 
 
          if "file" not in request.files:
-              return redirect(request.url)
+              return redirect(request.url) #check whether wav file is added or not
         
          file = request.files["file"]
          if file.filename == "":
-             return redirect(request.url)
+             return redirect(request.url)  #checking whether proper file is added
         
          if file:
-             recognizer = sr.Recognizer()
+             recognizer = sr.Recognizer()  #calling recognizer method
              wavfile = sr.AudioFile(file)
              with wavfile as source:
                  data = recognizer.record(source)
-             transcript = recognizer.recognize_google(data, key=None)
+             transcript = recognizer.recognize_google(data, key=None) #processing wavfile here
              
-
+# render data 
     return render_template('index.html' , transcript = transcript)
 
 if __name__ == " main ":
